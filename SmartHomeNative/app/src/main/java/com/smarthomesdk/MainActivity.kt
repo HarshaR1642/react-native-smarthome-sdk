0abc9ace250932a18c6921d13cef9c219120ce08
HarshaR1642 <sriharsha@rently.com>
2025-03-18 00:54:49 +0530

    Add screen

--- SmartHomeNative/app/src/main/java/com/smarthomesdk/MainActivity.kt
+++ SmartHomeNative/app/src/main/java/com/smarthomesdk/MainActivity.kt
@@ -3,14 +3,38 @@ package com.smarthomesdk
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        val intent = Intent(this, SDKMainActivity::class.java)
-        startActivity(intent)
+        setContent { App() }
     }
+}
+
+@Composable
+fun App() {
+  val activity = LocalActivity.current
+
+  Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Button(
+      colors = ButtonDefaults.buttonColors().copy(containerColor = Color.Black),
+      onClick = {
+        val intent = Intent(activity, SDKMainActivity::class.java)
+        activity?.startActivity(intent)
+      }) {
+      Text("Open Smart Home")
+    }
+  }
 }
